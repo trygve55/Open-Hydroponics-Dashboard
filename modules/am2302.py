65b342775196b9bb0d58a9dadbd2e221c889c439
Trygve Nerland <trygve55@gmail.com>
2019-06-06 20:07:18 +0200

    Readded console logs

--- modules/am2302.py
+++ modules/am2302.py
@@ -8,11 +8,17 @@ def thread_sensor(self):
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, self.pin)
 
+        print(temperature, " ", humidity)
+
+        print(self.temperature, " ", self.humidity)
+
         if humidity is not None and temperature is not None:
+            print("test")
             self.humidity = humidity
             self.temperature = temperature
 
-        time.sleep(5)
+            print(self.temperature, " ", self.humidity)
+        time.sleep(3)
 
 
 class AM2302(SensorClass):
@@ -26,6 +32,7 @@ class AM2302(SensorClass):
         x.start()
 
     def get_state(self):
+        print(self.temperature, " ", self.humidity)
         return {
             "humidity": self.humidity,
             "temperature": self.temperature
