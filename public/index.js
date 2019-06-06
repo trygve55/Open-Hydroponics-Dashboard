badb0b975eb5c87753a7087e30396606b8ac5d1a
Trygve Nerland <trygve55@gmail.com>
2019-06-06 14:43:16 +0200

    Added support for multiple devices

--- public/index.js
+++ public/index.js
@@ -1,17 +1,48 @@
-function set_gpio(state) {
-   fetch('/api/gpio', {
-    method: 'POST',
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({state: state})
-  }).then(
-    response => response.json() // if the response is a JSON object
-  ).then(
-    response => console.log(response)
-  ).catch(
-    error => console.log(error) // Handle the error response object
-  );
+function set_gpio(device, state) {
+    fetch('/api/device', {
+        method: 'POST',
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            device: device,
+            state: state
+        })
+    }).then(
+        response => response.json() // if the response is a JSON object
+    ).then(
+        response => console.log(response)
+    ).catch(
+        error => console.log(error) // Handle the error response object
+    );
 }
+
+function get_devices(callback) {
+    fetch('/api/devices')
+    .then(response => response.json())
+    .then(json => callback(json), callback)
+}
+
+get_devices(function(devices) {
+    console.log(devices);
+
+    devices.forEach(function(device) {
+        console.log(device)
+
+        let table = document.getElementById("devices");
+        let row = table.insertRow();
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+
+        cell0.innerHTML = device.name;
+        cell1.innerHTML = "<button onclick=\"set_gpio('" + device.name + "',1)\">On</button>";
+        cell2.innerHTML = "<button onclick=\"set_gpio('" + device.name + "',0)\">Off</button>";
+    });
+
+
+});
+
+
--- server.py
+++ server.py
@@ -1,6 +1,23 @@
 from flask import Flask, jsonify, render_template, make_response, request, abort
-from modules.gpio_relay import GPIORelay
-import io
+import sys
+import json
+
+#Check if running on RPi
+if 'RPi' in sys.modules:
+    from modules.gpio_relay import GPIORelay
+else:
+    from modules.dummy_relay import GPIORelay
+
+###Set up devices start
+
+devices = dict()
+
+devices['relay0'] = GPIORelay(26)
+devices['relay1'] = GPIORelay(16)
+devices['relay2'] = GPIORelay(20)
+devices['relay3'] = GPIORelay(21)
+
+###Set up devices end
 
 ###Testing web interface endpoints start
 
@@ -35,16 +52,27 @@ def test_post():
     return jsonify({}), 200
 
 
-#GPIO test
-@app.route('/api/gpio', methods=['post'])
-def gpio_post():
+#Devices list
+@app.route('/api/devices', methods=['get'])
+def get_devices():
+    devices_list = list()
+    for k in devices.keys():
+        devices_list.append({'name': k})
 
-    test = GPIORelay(26)
+    return jsonify(devices_list), 200
+
+#Device
+@app.route('/api/device', methods=['post'])
+def device_post():
 
     data = request.get_json()
-    test.set_state(data['state'])
 
-    return jsonify({'state': test.get_state()}), 200
+    if data['device'] not in devices.keys():
+        return 404
+
+    devices[data['device']].set_state(data['state'])
+
+    return jsonify({'state': devices[data['device']].get_state()}), 200
 
 ###REST interface endpoints end
 
@@ -72,6 +100,6 @@ if __name__ == '__main__':
     }'''
 
     #app.run(debug=True, ssl_context=context, port=8443)                    #Enable this line for SSL(HTTPS)
-    app.run(debug=False, port=8080, host='0.0.0.0')                                          #Disable this line for SSL(HTTPS)
+    app.run(debug=True, port=8080, host='0.0.0.0')                                          #Disable this line for SSL(HTTPS)
 
 ####Runs server end
