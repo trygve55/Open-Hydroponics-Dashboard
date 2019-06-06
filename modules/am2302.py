357e48286925f7cfb5a43fc45e7c40109bb359e1
Trygve Nerland <trygve55@gmail.com>
2019-06-06 15:51:44 +0200

    Added threading to AM2302

--- modules/am2302.py
+++ modules/am2302.py
@@ -1,14 +1,28 @@
 from modules.module_templates.sensor_class import SensorClass
 import Adafruit_DHT
+import threading
+import time
+
+
+def thread_sensor(self):
+    self.humidity, self.temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, self.pin)
+    time.sleep(3)
+
 
 class AM2302(SensorClass):
 
     def __init__(self, pin):
         self.pin = pin
+        self.temperature = 0.0
+        self.humidity = 0.0
+
+        threading.Thread(target=thread_sensor, args=(self,))
 
     def get_state(self):
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, self.pin)
         return {
-            "humidity": humidity,
-            "temperature": temperature
+            "humidity": self.humidity,
+            "temperature": self.temperature
         }
+
+
