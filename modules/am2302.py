b5df0ec469fc61220a33a4433f9f2669bce3a910
Trygve Nerland <trygve55@gmail.com>
2019-06-06 15:52:25 +0200

    Fixed typo in code

--- modules/am2302.py
+++ modules/am2302.py
@@ -19,7 +19,6 @@ class AM2302(SensorClass):
         threading.Thread(target=thread_sensor, args=(self,))
 
     def get_state(self):
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, self.pin)
         return {
             "humidity": self.humidity,
             "temperature": self.temperature
